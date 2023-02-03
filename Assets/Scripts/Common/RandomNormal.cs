d3a6a3c614448dce4370790c5f4ee228880574a1
Kit Matthewson <kit.matthewson@gmail.com>
2023-02-03 11:02:07 +0000

    Doc comments

--- Assets/Scripts/Common/RandomNormal.cs
+++ Assets/Scripts/Common/RandomNormal.cs
@@ -3,6 +3,10 @@ using System;
 public class RandomNormal {
     private static readonly Random _rand = new();
 
+    /// <summary>
+    /// Generates a random normal with mu = 0, and standard deviation <c>std</c>
+    /// </summary>
+    /// <param name="std">Standard distribution.</param>
     public static double Random(double std) {
         double u1 = 1.0 - _rand.NextDouble();
         double u2 = 1.0 - _rand.NextDouble();
